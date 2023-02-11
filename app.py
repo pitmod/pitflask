cdddf7921581b783a3b94e14aff58059c5687933
pitmod <piotr.modlinger@gmail.com>
2023-02-11 13:01:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_pit():
-    return 'Hello Pit'
+    return 'Hello Piter. This is some test flask app.'
 
 if __name__ == '__main__':
     app.run()
